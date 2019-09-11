722ea40f0528122f167937944163d31aa0519650
Alexander Johnston <Aliics@hotmail.com>
2019-09-12 11:50:20 +1200

    Added removeCommas method

--- src/main/java/fish/eyebrow/gman/casing/AbstractFormat.java
+++ src/main/java/fish/eyebrow/gman/casing/AbstractFormat.java
@@ -2,6 +2,11 @@ package fish.eyebrow.gman.casing;
 
 abstract class AbstractFormat {
 
+    static String removeCommas(final String text) {
+        return text.replaceAll("'", "");
+    }
+
+
     static String capitalize(final String text) {
         return text.replace(text.charAt(0), Character.toUpperCase(text.charAt(0)));
     }
--- src/main/java/fish/eyebrow/gman/casing/PascalCaseFormat.java
+++ src/main/java/fish/eyebrow/gman/casing/PascalCaseFormat.java
@@ -3,6 +3,6 @@ package fish.eyebrow.gman.casing;
 public class PascalCaseFormat extends AbstractFormat {
 
     public static String format(final String text) {
-        return capitalize(text.toLowerCase());
+        return capitalize(removeCommas(text.toLowerCase()));
     }
 }
