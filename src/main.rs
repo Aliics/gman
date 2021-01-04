a98733105228c6ed62aba650497aca58379332c0
Alexander Johnston <Aliics@hotmail.com>
2021-01-04 17:29:23 +1300

    keep apostrophe arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg, ArgMatches};
+use clap::{App, Arg};
 use core::result;
 use rand::Rng;
 use std::io::{self, Read};
@@ -6,48 +6,59 @@ use std::{fs::File, num::ParseIntError};
 
 const DICT: &str = "/etc/dictionaries-common/words";
 
+#[derive(Default)]
 struct Conf {
     count: i32,
     delimiter: String,
+    keep_apostrophe: bool,
 }
 
 impl Conf {
-    fn of_arg_matches(am: ArgMatches) -> result::Result<Conf, ParseIntError> {
+    fn build() -> result::Result<Conf, ParseIntError> {
+        let am = App::new("gman")
+            .version("0.1.0")
+            .args(&[
+                Arg::with_name("count")
+                    .help("amount of words to be generated")
+                    .short("c")
+                    .long("count")
+                    .takes_value(true)
+                    .default_value("1"),
+                Arg::with_name("delimiter")
+                    .help("word delimiter")
+                    .short("d")
+                    .long("delimiter")
+                    .takes_value(true)
+                    .default_value(" "),
+                Arg::with_name("keep_apostrophe")
+                    .help("keep the apostrophes of dictionary words")
+                    .short("k")
+                    .long("keep-apostrophe"),
+            ])
+            .get_matches();
         let count = am.value_of("count").unwrap().parse()?;
         let delimiter = am.value_of("delimiter").unwrap().to_owned();
-        Ok(Conf { count, delimiter })
+        let keep_apostrophe = am.is_present("keep_apostrophe");
+        Ok(Conf {
+            count,
+            delimiter,
+            keep_apostrophe,
+        })
     }
 }
 
 fn main() -> io::Result<()> {
-    let am = App::new("gman")
-        .version("0.1.0")
-        .args(&[
-            Arg::with_name("count")
-                .help("amount of words to be generated")
-                .short("c")
-                .long("count")
-                .takes_value(true)
-                .default_value("1"),
-            Arg::with_name("delimiter")
-                .help("word delimiter")
-                .short("d")
-                .long("delimiter")
-                .takes_value(true)
-                .default_value(" "),
-        ])
-        .get_matches();
-    let Conf { count, delimiter } = Conf::of_arg_matches(am).unwrap();
+    let conf = Conf::build().unwrap();
     let mut f = File::open(DICT)?;
     let mut data = Vec::new();
     f.read_to_end(&mut data)?;
-    let words = group_words(data);
+    let words = group_words(&conf, data);
     let mut rng = rand::thread_rng();
-    for i in 0..count {
+    for i in 0..conf.count {
         let line = rng.gen_range(0..words.len());
         let word = words[line].as_str();
-        let delimiter = if i < count - 1 {
-            delimiter.as_str()
+        let delimiter = if i < conf.count - 1 {
+            conf.delimiter.as_str()
         } else {
             "\n"
         };
@@ -56,15 +67,19 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn group_words(bytes: Vec<u8>) -> Vec<String> {
+fn group_words(conf: &Conf, bytes: Vec<u8>) -> Vec<String> {
     let mut word = String::new();
     let mut words = Vec::new();
     for b in bytes {
-        if b as char == '\n' {
+        let b = b as char;
+        if b == '\n' {
             words.push(word);
             word = String::new();
         } else {
-            word.push(b as char);
+            if !conf.keep_apostrophe && b == '\'' {
+                continue;
+            }
+            word.push(b);
         }
     }
     words
@@ -72,12 +87,12 @@ fn group_words(bytes: Vec<u8>) -> Vec<String> {
 
 #[cfg(test)]
 mod tests {
-    use crate::group_words;
+    use crate::{group_words, Conf};
 
     #[test]
     fn grouped() {
         let separated: Vec<u8> = vec![b'a', b'b', b'c', b'\n', b'd', b'e', b'f', b'\n'];
-        let words = group_words(separated);
+        let words = group_words(&Conf::default(), separated);
         assert_eq!(words, vec!["abc", "def"]);
     }
 }
