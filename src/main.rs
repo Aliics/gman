187b77bd65ddfb026cfb649a9094d5d97455910f
Alexander Johnston <Aliics@hotmail.com>
2021-01-04 17:36:51 +1300

    upper and lower case args

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ struct Conf {
     count: i32,
     delimiter: String,
     keep_apostrophe: bool,
+    lower_case: bool,
+    upper_case: bool,
 }
 
 impl Conf {
@@ -34,15 +36,22 @@ impl Conf {
                     .help("keep the apostrophes of dictionary words")
                     .short("k")
                     .long("keep-apostrophe"),
+                Arg::with_name("lower_case")
+                    .help("make all words lower cased")
+                    .short("l")
+                    .long("lower-case"),
+                Arg::with_name("upper_case")
+                    .help("make all words upper cased")
+                    .short("u")
+                    .long("upper-case"),
             ])
             .get_matches();
-        let count = am.value_of("count").unwrap().parse()?;
-        let delimiter = am.value_of("delimiter").unwrap().to_owned();
-        let keep_apostrophe = am.is_present("keep_apostrophe");
         Ok(Conf {
-            count,
-            delimiter,
-            keep_apostrophe,
+            count: am.value_of("count").unwrap().parse()?,
+            delimiter: am.value_of("delimiter").unwrap().to_owned(),
+            keep_apostrophe: am.is_present("keep_apostrophe"),
+            lower_case: am.is_present("lower_case"),
+            upper_case: am.is_present("upper_case"),
         })
     }
 }
@@ -79,7 +88,13 @@ fn group_words(conf: &Conf, bytes: Vec<u8>) -> Vec<String> {
             if !conf.keep_apostrophe && b == '\'' {
                 continue;
             }
-            word.push(b);
+            word.push(if conf.lower_case {
+                b.to_ascii_lowercase()
+            } else if conf.upper_case {
+                b.to_ascii_uppercase()
+            } else {
+                b
+            });
         }
     }
     words
